8999af6d73ca16bf24443363354e38ce407dd5e2
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-01-24 19:48:48 +0100

    button not working

--- js/main.js
+++ js/main.js
@@ -12,43 +12,55 @@ const discount_20 = 0.2;
 const discount_40 = 0.4;
 
 // CHIEDI NOME 
-const nome_cognome_input_el = document.getElementById('nome_cognome');
-const nome_cognome = nome_cognome_input_el.value;
+let nome_cognome_input_el = document.getElementById('nome_cognome');
+let nome_cognome = nome_cognome_input_el.value;
 
 // CHIEDI KM 
-const kilometri_input_el = document.getElementById('kilometri');
-const kilometri = parseInt(kilometri_input_el.value);
+let kilometri_input_el = document.getElementById('kilometri');
+let kilometri = parseInt(kilometri_input_el.value);
 
 // CHIEDI ETA'
-const age_input_el = document.getElementById('age');
-const age = age_input_el.value;
+let age_input_el = document.getElementById('age');
+let age = age_input_el.value;
 console.log("age " + age);
 // PREZZO INTERO 
-const total_price = price_per_km * kilometri;
+let total_price = price_per_km * kilometri;
 let final_price = total_price
 // console.log('prezzo base ' + final_price);
 
-switch (age) {
-    case "maggiorenne":
-        final_price = total_price
-        console.log('prezzo maggiorenne base ' + final_price);
-        document.getElementById('passeggero').innerHTML = ('Biglietto standard');
-        break;
 
-    case "minorenne":
-        final_price = (total_price - (total_price * discount_20));
-        console.log('prezzo minorenne ' + final_price);
-        document.getElementById('passeggero').innerHTML = ('Offerta Minorenni');
-        break;
-
-    case "over65":
-        final_price = (total_price - (total_price * discount_40));
-        console.log('prezzo over65: ' + final_price);
-        document.getElementById('passeggero').innerHTML = ('Offerta Over65');
-        break;
-}
 const calculate_button = document.querySelector(".btn-primary")
 console.log(calculate_button);
-document.getElementById('final_price').innerHTML = (final_price.toFixed(2) + '€');
+calculate_button.addEventListener(
+    'click',
+    function () {
+        if (isNaN(kilometri)) {
+            alert("immettere correttamente i Km da percorrere")
+        }
+        else {
+            switch (age) {
+                case "maggiorenne":
+                    final_price = total_price
+                    console.log('prezzo maggiorenne base ' + final_price);
+                    document.getElementById('offerta').innerHTML = ('Biglietto standard');
+                    break;
+
+                case "minorenne":
+                    final_price = (total_price - (total_price * discount_20));
+                    console.log('prezzo minorenne ' + final_price);
+                    document.getElementById('offerta').innerHTML = ('Offerta Minorenni');
+                    break;
+
+                case "over65":
+                    final_price = (total_price - (total_price * discount_40));
+                    console.log('prezzo over65: ' + final_price);
+                    document.getElementById('offerta').innerHTML = ('Offerta Over65');
+                    break;
+            }
+            document.getElementById('final_price').innerHTML = (final_price.toFixed(2) + '€');
+            document.getElementById('passeggero').innerHTML = nome_cognome;
+        }
+    }
+)
 
 

dea5b46cca5736d04913395ed3d99a0d51b9c7b6
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-01-24 18:11:21 +0100

    richieste funzionanti inizio prova switch

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// 1.chiedi all' utente nome e cognome
+// 2.chiedi all' utente il numero di km che vuole percorrere
+// 3.chiedi se è minorenne, maggiorenne, over65
+// 4.calcola il prezzo intero del viaggio
+// 5. se passeggero è minorenne applica sconto 20%
+// 6. se passeggero è over65 applica sconto 40%
+// 7. altrimenti il prezzo del biglietto è intero
+
+// PREZZO PER KM 
+const price_per_km = 0.21;
+const discount_20 = 0.2;
+const discount_40 = 0.4;
+
+// CHIEDI NOME 
+const nome_cognome_input_el = document.getElementById('nome_cognome');
+const nome_cognome = nome_cognome_input_el.value;
+
+// CHIEDI KM 
+const kilometri_input_el = document.getElementById('kilometri');
+const kilometri = parseInt(kilometri_input_el.value);
+
+// CHIEDI ETA'
+const age_input_el = document.getElementById('age');
+const age = age_input_el.value;
+
+// PREZZO INTERO 
+const total_price = price_per_km * kilometri;
+const final_price = ;
+switch (age) {
+    case maggiorenne 
+
+}
